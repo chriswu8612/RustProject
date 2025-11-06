eaca890981441956128e1d744a2b7161e5f8f5a8
Christopher Wu <chriswu8612@yahoo.com>
2025-11-06 16:07:30 -0600

    module

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,10 @@
+mod guess_module;
 
-use std::{cmp::Ordering, io};
-use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-    let secret_number = rand::rng().random_range(1..=100);
-
-    println!("Secret number is {secret_number}");
-
-    loop {
-        println!("Please input your guess:");
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-        println!("You guessed: {guess}");
-
-        let guess: i32  = guess.trim().parse().expect("Please type a number!");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    guess_module::guess();
 }
+
+
+
+
