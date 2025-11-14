f9556d044603a8108e9151acf4b69ce9767cf168
Christopher Wu <chriswu8612@yahoo.com>
2025-11-14 13:27:35 -0600

    struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod slice_module;
+mod struct_module;
 
 // next: https://doc.rust-lang.org/book/ch05-00-structs.html
 fn main() {
-    slice_module::slice_array_fn();
+    struct_module::rect_fn();
 }
 
 
