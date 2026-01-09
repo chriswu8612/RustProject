78816d592dfe4ed73f1b86fbd1700b61e6b1ad5a
Christopher Wu <chriswu8612@yahoo.com>
2026-01-09 12:47:34 -0600

    updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod match_module;
 
-// next: https://doc.rust-lang.org/book/ch07-00-managing-growing-projects-with-packages-crates-and-modules.html
+// next: https://doc.rust-lang.org/book/ch07-03-paths-for-referring-to-an-item-in-the-module-tree.html
 fn main() {
    match_module::display_coin_match();
 }
