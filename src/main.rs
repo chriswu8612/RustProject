40463af6fcf3a49bbec7bcba751e7579bc168133
Christopher Wu <chriswu8612@yahoo.com>
2026-01-30 14:47:58 -0600

    module use

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use RustProject::{back_of_house::{eat_at_restaurant2, fix_incorrect_order}, eat_at_restaurant};
+// use RustProject::{back_of_house::{eat_at_restaurant2, fix_incorrect_order}, eat_at_restaurant};
+use RustProject::back_of_house::eat_at_restaurant2;
+use RustProject::back_of_house::fix_incorrect_order;
+use RustProject::eat_at_restaurant;
 
 // next: https://doc.rust-lang.org/book/ch07-04-bringing-paths-into-scope-with-the-use-keyword.html
 fn main() {
